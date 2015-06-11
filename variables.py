b360c6dd720ed45e99be5af1b62bdd2d5e618d3a
gen <xgen.osama@gmail.com>
2015-06-11 22:10:15 +0200

    updated 3

--- variables.py
+++ variables.py
@@ -1,5 +1,6 @@
 __author__ = 'gen'
 
+import math
 
 x = 18
 
@@ -8,6 +9,16 @@ x += 15
 g = input('Enter number here : ')
 
 g **= 30
+
+print(pow(5, 4))
+abs(-18)
+math.floor(18.7)
+math.sqrt(81)
+
+bucky = math.sqrt
+
+bucky(3)
+
 print(x)
 print(g)
 
