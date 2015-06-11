613cbdc5798749176d5b1b7a726eb02b246c5e78
gen <xgen.osama@gmail.com>
2015-06-12 00:09:34 +0200

    updated 6

--- lists.py
+++ lists.py
@@ -29,4 +29,39 @@ print(example[10:0:-2])
 print(example[::-2])
 # [9, 7, 5, 3, 1]
 
+example2 = [10, 30]
+print(example + example2)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30]
+
+example3 = ['osama', 'mohamed']
+
+print(example2 + example3)
+# [10, 30, 'osama', 'mohamed']
+
+
+print(example3[0] * 10)
+# osamaosamaosamaosamaosamaosamaosamaosamaosamaosama
+
+print([21] * 10)
+
+# [21, 21, 21, 21, 21, 21, 21, 21, 21, 21]
+
+name = 'ahmed'
+
+print('z' in name)
+# False
+print('a' in name)
+# True
+
+family2 = ['mom', 'dad', 'bro']
+
+print('mom' in family2)
+# True
+print('sis' in family2)
+# False
+
+
+
+
+
 
