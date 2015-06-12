e810acdab05226fb2df68becf37983f4e109caa1
gen <xgen.osama@gmail.com>
2015-06-12 14:18:00 +0200

    updated 107

--- working_with_files.py
+++ working_with_files.py
@@ -15,8 +15,18 @@ __author__ = 'gen'
 # fob.write('\ng33k here')
 # fob.close()
 
-fob = open('test.txt', 'r')
-listLines = fob.readlines()
-
-print listLines
+# fob = open('test.txt', 'r')
+# listLines = fob.readlines()
+#
+# print listLines
+#
+# listLines[2] = '\n i\'m g33k'
+#
+# print listLines
+fob = open('test.txt', 'w')
+write = ['\nosama', '\nabdo']
+fob.writelines(write)
+# listLines2 = fob.readlines()
+# print listLines2
 fob.close()
+
