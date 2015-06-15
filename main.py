d565ad15ba4c7b07842d63755f3fbdb93438b193
xgenOsama <xgen.osama@gmail.com>
2015-06-15 21:19:32 +0200

    full load

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import os
 import thread
 import datetime
+import time
 def main():
     print(
     'hello'
@@ -25,13 +26,30 @@ def main():
     cop = persons[0].copy()
     persons[0] = None
     print(cop)
+
     mythread = thread
+
     def printDate():
         print(datetime.date.today())
 
     printDate()
 
+    def print_time(threadName, delay):
+        count = 0
+        while count < 5:
+            time.sleep(delay)
+            count += 1
+            print "%s: %s" % (threadName, time.ctime(time.time()))
 
+    # Create two threads as follows
+    try:
+       mythread.start_new_thread(print_time, ("Thread-1", 2, ))
+       mythread.start_new_thread(print_time, ("Thread-2", 4, ))
+    except:
+       print "Error: unable to start thread"
+
+    while 1:
+       pass
 if __name__ == '__main__':
     main()
 
