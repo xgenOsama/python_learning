336d490fd77dd24b638f7699c2b5b3bbf909a435
xgenOsama <xgen.osama@gmail.com>
2015-06-15 21:10:55 +0200

    final commit

--- main.py
+++ main.py
@@ -2,18 +2,34 @@ __author__ = 'gen'
 
 import sys
 import os
+import thread
+import datetime
 def main():
     print(
     'hello'
     )
     print 'hello i am osama'
     os.system('ls')
-    os.system('ping google.com ')
+    os.system('ping google.com -c 3')
     persons = [{'name': 'osama', 'age': 13}, {'name': 'mohamed', 'age': 16}]
     print(persons[0]['name'])
     print(persons[0]['age'])
     print(persons[1]['name'])
     print(persons[1]['age'])
+    persons.insert(2, {'name': 'ahmed', 'age': 11})
+    for s in persons:
+        print(s)
+    print(persons[0]. has_key('name'))
+    print(persons[0].viewkeys())
+    print(persons[0].items())
+    cop = persons[0].copy()
+    persons[0] = None
+    print(cop)
+    mythread = thread
+    def printDate():
+        print(datetime.date.today())
+
+    printDate()
 
 
 if __name__ == '__main__':
