83931affc0a60847284e6c386d9461499a16d21a
xgenOsama <xgen.osama@gmail.com>
2015-08-27 04:03:30 +0200

    sendning emails commited

---
+++ sendmail.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# from email.MIMEMultipart import MIMEMultipart
+# from email.MIMEText import MIMEText
+
+
+fromaddr = 'xgen.osama@gmail.com'
+toaddr = 'xgen.osama@gmail.com'
+text = 'text message sent from python'
+username = 'xgenOsama'
+password = 'javag33kprohunter'
+
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = 'Text'
+msg.attach(MIMEText(text))
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username, password)
+server.sendmail(fromaddr, toaddr, msg.as_string())
+server.quit()
